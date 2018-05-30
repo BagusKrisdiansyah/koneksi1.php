eb208f82c59569e37d38a73f734264a0afbe7c10
BagusKrisdiansyah <baguskrisdiansyah@gmail.com>
2018-05-31 01:14:58 +0700

    Create koneksi1.php

---
+++ koneksi1.php
@@ -0,0 +1,17 @@
+<?php
+
+$host = "localhost";
+
+$user = "root";
+
+$pass = "";
+
+$name = "tari";
+
+
+
+$koneksi = mysql_connect ($host, $user, $pass) or die ("Koneksi Ke Database Gagal");
+
+mysql_select_db ($name, $koneksi) or die ("Tidak ada database yang dipilih");
+
+?>
